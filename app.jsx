f4e429e1e519c02aa56a4af19a3d7425a109428a
Kevin Johnston <kjohnston1149@gmail.com>
2017-05-08 16:06:07 -0700

    Add tick function to print elapsed time

--- app.jsx
+++ app.jsx
@@ -23,15 +23,39 @@ var Stopwatch = React.createClass({
   getInitialState: function() {
     return {
       running: false,
+      elapsedTime: 0,
+      previousTime: 0,
     }
   },
 
-  onStart: function() {
+  componentDidMount: function() {
+    this.interval = setInterval(this.onTick, 100);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
+  onTick: function () {
+    if (this.state.running) {
+      var now = Date.now();
+      this.setState({
+        previousTime: now,
+        elapsedTime: this.state.elapsedTime + (now - this.state.previousTime)
+      });
+    }
 
   },
 
-  onStop: function() {
+  onStart: function() {
+    this.setState({
+      running: true,
+      previousTime: Date.now(),
+     });
+  },
 
+  onStop: function() {
+    this.setState({ running: false });
   },
 
   onReset: function() {
@@ -39,12 +63,13 @@ var Stopwatch = React.createClass({
   },
 
   render: function() {
+    var seconds = Math.floor(this.state.elapsedTime / 1000);
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <div className="stopwatch-time">0</div>
+        <div className="stopwatch-time">{seconds}</div>
         {this.state.running ?
-          <button onclick={this.onStop}>Stop</button>
+          <button onClick={this.onStop}>Stop</button>
           :
           <button onClick={this.onStart}>Start</button>
         }
