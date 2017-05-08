d67179e6a3a33c1a4831d45ef1250d218eb61845
Kevin Johnston <kjohnston1149@gmail.com>
2017-05-08 15:03:48 -0700

    Create stopwatch component with static structure

--- app.jsx
+++ app.jsx
@@ -18,6 +18,19 @@ var PLAYERS = [
 
 var nextId = 4;
 
+var Stopwatch = React.createClass({
+  render: function() {
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <div className="stopwatch-time">0</div>
+        <button>Start</button>
+        <button>Reset</button>
+      </div>
+    );
+  }
+});
+
 var AddPlayerForm= React.createClass({
   propTypes: {
     onAdd: React.PropTypes.func.isRequired,
@@ -81,6 +94,7 @@ function Header(props) {
     <div className="header">
       <Stats players={props.players}/>
       <h1>{props.title}</h1>
+      <Stopwatch />
     </div>
   );
 }
@@ -172,7 +186,7 @@ var Application = React.createClass({
   onRemovePlayer: function(index) {
     this.state.players.splice(index, 1);
     this.setState(this.state);
-  
+
   },
 
   render: function() {
